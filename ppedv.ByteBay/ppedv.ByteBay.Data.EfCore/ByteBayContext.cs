0424868d572144640b6f16ab8f6c8e96a5d697fd
André <AndreR@ppedv.de>
2023-05-25 11:59:20 +0200

    DbContext

---
+++ ppedv.ByteBay/ppedv.ByteBay.Data.EfCore/ByteBayContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.ByteBay.Model;
+using System.Diagnostics;
+
+namespace ppedv.ByteBay.Data.EfCore
+{
+    public class ByteBayContext : DbContext
+    {
+        public DbSet<Adresse> Adressen { get; set; }
+        public DbSet<BestellPosition> BestellPositionen { get; set; }
+        public DbSet<Bestellung> Bestellung { get; set; }
+        public DbSet<Lieferant> Lieferanten { get; set; }
+        public DbSet<Produkt> Produkte { get; set; }
+
+
+        readonly string conString;
+
+        public ByteBayContext(string conString)
+        {
+            this.conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(conString)
+                          .UseLazyLoadingProxies()
+                          .LogTo(x => Debug.WriteLine(x))
+                          .EnableSensitiveDataLogging()
+                          .EnableDetailedErrors();
+        }
+    }
+}
